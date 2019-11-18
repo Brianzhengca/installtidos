901acd0b9bad7e44654123a64f7bd96a3902e511
Brian <30914529+Brianzhengca@users.noreply.github.com>
2019-11-18 10:52:41 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 apt-get update
-apt-get install python
+apt-get install python python-pip
+apt-get install libmysqlclient-dev
+apt-get install python-dev
+apt-get install python-dev  \
+     build-essential libssl-dev libffi-dev \
+     libxml2-dev libxslt1-dev zlib1g-dev \
+     python-pip
+pip install mysql-python
 apt-get install gcc
 git clone https://github.com/0xinfection/tidos-framework.git
 cd tidos-framework/docker
